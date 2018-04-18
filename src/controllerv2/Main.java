9c1aea42d42198c547346eb6c4d139b1f8274c6f
Adrián-Trabajo <Adrin-Trabajo@AdrinTrabajo>
2018-04-18 12:58:45 +0200

    Implementado el main teniendo que elegir los ficheros de entrada. Mejorado el control de excepciones, si se da una, termina la ejecución. Agregada una pausa al final de la ejecución para poder ver el resultado.

--- src/controllerv2/Main.java
+++ src/controllerv2/Main.java
@@ -13,8 +13,6 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JFrame;
-import javax.swing.JOptionPane;
 
 /**
  *
